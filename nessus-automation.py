fd16d6e075d3e25d7d24489cd1e03e679a7939da
Hamza AitBamoh <hamzaaitbamoh@gmail.com>
2023-08-05 19:10:19 +0100

    v1.1

--- nessus-automation.py
+++ nessus-automation.py
@@ -6,7 +6,21 @@ import ssl
 import pandas as pd
 import io
 import argparse
+import warnings
 
+#warnings.filterwarnings("ignore", message="Unverified HTTPS request is being made to host 'kali'.*")
+
+
+
+
+def printFolderMenu(folders):
+    for folder in folders:
+        print (folder.get("id"), "--", folder.get("name") )
+
+def printScanMenu(scans,folder_id):
+    for scan in scans:
+        if(scan.get("folder_id") == folder_id):
+            print (scan.get("id"), "--", scan.get("name") )
 
 # Print message on stdout
 def printMessage(msg, flag=1):
@@ -20,8 +34,6 @@ def printMessage(msg, flag=1):
         print(msg)
 
 # Check response code for an HTTP Response and print req message
-
-
 def checkStatus(resp, status_msg, error_msg):
     if resp.status_code == 200:
         printMessage(status_msg, 1)
@@ -34,43 +46,80 @@ def checkStatus(resp, status_msg, error_msg):
 def main():
     parser = argparse.ArgumentParser(description="A python script for automating exporting from nessus server to excel",
                                      epilog="written by 0x0h3mz4",
-                                     prog='python script.py', usage='%(prog)s -i <127.0.0.1> -u <nessus_user> -p <nessus_pass> ')
+                                     prog="python script.py", usage="%(prog)s -i <127.0.0.1> -u <nessus_user> -p <nessus_pass> ")
 
-    parser.add_argument(
-        "-i", "--server", help="IP[:PORT] of nessus server", required=True)
-    parser.add_argument(
-        "-u", "--user", help="username of nessus server", required=True)
-    parser.add_argument(
-        "-p", "--passwd", help="password of nessus server", required=True)
-    args = parser.parse_args()
+    parser.add_argument("-i", "--server", help="IP[:PORT] of nessus server", required=True)
+    parser.add_argument("-u", "--user", help="username of nessus server", required=True)
+    parser.add_argument("-p", "--passwd", help="password of nessus server", required=True)
+    args = parser.parse_args() 
 
     if ":" in args.server:
         ip = args.server.split(":")[0]
         port = args.server.split(":")[1]
+        print("ip: "+ip+"\nport: "+port)
     else:
         ip = args.server
         port = "8834"
 
-    base_url = "https://" + ip + ":" + port
-    creds = {'username': args.user, 'password': args.passwd}
-    #creds = {'username': '0x0h3mz4', 'password': 'n3ssUs1010%%'}
+    base_url = "http://" + ip + ":" + port
+    creds = {"username": args.user, "password": args.passwd}
+    #creds = {"username": "0x0h3mz4", "password": "n3ssUs1010%%"}
 
-    response1 = requests.post(base_url + "/session", creds, verify=False)
+    response1 = requests.post(base_url + "/session",creds,verify=False)
     if checkStatus(response1, "Login successful", "Invalid Login credentials"):
         token = json.loads(response1.text)
-        response2 = requests.get(
-            base_url + "/scans", headers={'X-Cookie': 'token=' + token['token']}, verify=False)
+        response2 = requests.get(base_url + "/scans", headers={"X-Cookie": "token=" + token["token"]},verify=False)
         if checkStatus(response2, "Fetching scans data\n", "Unable to fetch nessus scan"):
-            scans = json.loads(response2.text)
-            # test vulnerabilities critisisity
-            vuln = requests.get(
-                base_url + "/analysis", headers={'X-Cookie': 'token=' + token['token']}, verify=False)
-            vulnscan = json.loads(vuln.text)
-            # print(json.dumps(scans))
-            # convert to excel
-            df_json = pd.io.json.json_normalize(scans["scans"])
-            df_json.to_excel('DATAFILE.xlsx')
+            allscans = json.loads(response2.text)
 
+            printFolderMenu(allscans["folders"])
+            print("\n")
+            folder_id = int(input("Enter the folder you want : "))
+            print("\n \n")
+            printScanMenu(allscans["scans"],folder_id)
+            print("\n")
+            scan = int(input("Enter the scan you want : "))
+            response3 = requests.get(base_url + "/scans/"+str(scan), headers={"X-Cookie": "token=" + token["token"]},verify=False)
+            if checkStatus(response1, "Fetching data from the choosen scan", "Unable to fetch the choosen scan"):
+                
+                    scans = json.loads(response3.text)
+                    file=json.dumps(scans)
+                    fhosts=json.dumps(scans["hosts"])
+                    fvulnerabilities=json.dumps(scans["vulnerabilities"])
+                    f = open("allscans.json", "a")
+                    f.write(file)
+                    f.close()
+                    f = open("hosts.json", "a")
+                    f.write(fhosts)
+                    f.close()
+                    f = open("vulnerabilities.json", "a")
+                    f.write(fvulnerabilities)
+                    f.close()
+                    finalList=[]
+                    l = [dict(zip(["vulnerabilities"],[x.get("plugin_name")])) for x in scans["vulnerabilities"]]
+                    for i in l:
+                        i["adress_ip"]=[]
+                    for j in l:
+                        j["recommendation"]=[]
+                    
+    
+                    for host in scans["hosts"]:
+                    
+                        response4 = requests.get(base_url + "/scans/"+str(scan)+"/hosts/"+str(host.get("host_id")), headers={"X-Cookie": "token=" + token["token"]},verify=False)
+                        #if checkStatus(response4, "nice", "nope"):
+                        vuln_host = json.loads(response4.text)
+                        for i in l:
+                            for vuln in vuln_host.get("vulnerabilities"):
+                                if(i.get("vulnerabilities") == vuln.get("plugin_name")):
+                                    i["adress_ip"].append([host.get("hostname")])
+                            
 
-if __name__ == '__main__':
+                    
+                    #convert to excel
+                    df_json = pd.json_normalize(l)
+                    df_json.to_excel("DATAFILE.xlsx")
+
+                    print("check your folder for the file DATAFILE.xlsx")
+
+if __name__ == "__main__":
     main()
